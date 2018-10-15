0735e7b26087efab13d16b530fd588b764d67546
Yang Dai <yangdai713@aim.com>
2018-10-15 16:35:52 -0400

    modified utils functions

--- Week3/re-invent-the-wheel/lib/utils.py
+++ Week3/re-invent-the-wheel/lib/utils.py
@@ -6,8 +6,6 @@ helper functions for Logistic Regression.
 
 
 import numpy as np
-import h5py
-import matplotlib.pyplot as plt
 
 
 def sigmoid(z):
@@ -64,46 +62,3 @@ def initialize_with_zeros(n_features):
     assert(isinstance(b, float) or isinstance(b, int))
 
     return w, b
-
-
-def load_dataset():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:])
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:])
-
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:])
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:])
-
-    classes = np.array(test_dataset["list_classes"][:])
-
-    train_set_y_orig = train_set_y_orig.reshape(train_set_y_orig.shape[0])
-    test_set_y_orig = test_set_y_orig.reshape(test_set_y_orig.shape[0])
-
-    return (train_set_x_orig,
-            train_set_y_orig,
-            test_set_x_orig,
-            test_set_y_orig,
-            classes)
-
-
-def plot_image(X, y, index):
-    """
-    Visualize an example image in the dataset.
-    """
-    plt.imshow(X[index])
-    plt.axis('off')
-    plt.show()
-
-
-def describe_data(X_train_orig, y_train, X_test_orig, y_test):
-    n_samples_train = X_train_orig.shape[0]
-    n_samples_test = X_test_orig.shape[0]
-    print('-' * 50)
-    print("Number of training samples: {}".format(n_samples_train))
-    print("Number of testing samples: {}".format(n_samples_test))
-    print("X_train_orig shape: {}".format(X_train_orig.shape))
-    print("y_train shape: {}".format(y_train.shape))
-    print("X_test_orig shape: {}".format(X_test_orig.shape))
-    print("y_test shape: {}".format(y_test.shape))
-    print('-' * 50)
