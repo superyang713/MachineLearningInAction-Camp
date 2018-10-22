8a35f5fa1027708ea0b6918da244458c87f3e903
Yang Dai <yangdai713@aim.com>
2018-10-22 17:40:53 -0400

    refactor the svm classifier

--- Week3/re-invent-the-wheel/lib/svm.py
+++ Week3/re-invent-the-wheel/lib/svm.py
@@ -75,6 +75,7 @@ class SVMClassifier:
 
     """
     def __init__(self, kernel='linear', C=1.0, epsilon=0.1, tol=1e-3):
+        # Hyperparameters
         self.C = C
         self.kernel = kernel
         self.epsilon = epsilon
@@ -141,16 +142,46 @@ class SVMClassifier:
                     if examine_result:
                         objective = self._objective_function(self.alphas)
                         self._obj.append(objective)
+
             if examine_all == 1:
                 examine_all = 0
+
             elif num_changed == 0:
                 examine_all = 1
 
         return self
 
     def predict(self, X):
+        '''
+        Predict whether the label is 0 or 1 based on the result from decsion
+        function.
+
+        Parameters:
+        ----------
+        X : data, a numpy array of size (n_samples, n_features)
+
+        Returns:
+        -------
+        y_predict : a numpy array (vector) containing all predictions.
+        '''
+
+        decision = self.decision_function(X)
+        y_predict = np.sign(decision)
+        y_predict[y_predict == -1] = 0
+
+        return y_predict
+
+    def decision_function(self, X):
         """
-        Applies the SVM decision function to the input vectors X_test
+        Applies the SVM decision function to the data X.
+
+        parameters:
+        ----------
+        X : data, a numpy array of size (n_samples, n_features)
+
+        return:
+        ----------
+        result : a numpy array (vector) with the shape of (n_samples,)
         """
 
         if self.kernel == 'linear':
@@ -176,39 +207,71 @@ class SVMClassifier:
         ----------
         self : object
         """
-        self.alphas = np.zeros(self.X.shape[0])
 
-    def _linear_kernel(self, X, Y):
+        self.alphas = np.zeros(self.X.shape[0])
+        return self
+
+    def _init_error(self, X):
         """
-        Returns the linear combination of arrays x and y with the optional bias
-        term `b` (set to 1 by default).
+        Initialize a vector of errors.
+
+        parameters:
+        ----------
+        X : data, a numpy array of size (n_samples, n_features)
+
+        return:
+        ----------
+        self : object
         """
-        result = np.dot(X, Y.T) + self.b
+
+        self.errors = self.decision_function(X) - self.y
+        return self
+
+    def _linear_kernel(self, X1, X2):
+        """
+        Returns the linear combination of arrays X1 and X2 with the kernal
+        scalar b.
+
+        parameters:
+        ----------
+        X1 : data, a numpy array of size (n_samples, n_features)
+        X2 : data, a numpy array of size (n_samples, n_features)
+
+        returns:
+        -------
+        result : kernelized vector dot production in the dual form.
+        """
+        result = np.dot(X1, X2.T) + self.b
 
         return result
 
-    def _init_error(self, X):
-        self.errors = self.predict(X) - self.y
-        return self
-
-    def _gaussian_kernel(self, X, Y):
+    def _gaussian_kernel(self, X1, X2):
         """
-        Returns the gaussian similarity of arrays x and y with kernel width
+        Returns the gaussian similarity of arrays X1 and X2 with kernel width
         parameter sigma.
+
+        parameters:
+        ----------
+        X1 : data, a numpy array of size (n_samples, n_features)
+        X2 : data, a numpy array of size (n_samples, n_features)
+
+        returns:
+        -------
+        result : kernelized vector dot production in the dual form.
         """
 
-        if np.ndim(X) == 1 and np.ndim(Y) == 1:
-            result = np.exp(- np.linalg.norm(X - Y) / (2 * self.sigma ** 2))
+        if np.ndim(X1) == 1 and np.ndim(X2) == 1:
+            result = np.exp(- np.linalg.norm(X1 - X2) / (2 * self.sigma ** 2))
 
-        elif ((np.ndim(X) > 1 and np.ndim(Y) == 1) or
-              (np.ndim(X) == 1 and np.ndim(Y) > 1)):
+        elif ((np.ndim(X1) > 1 and np.ndim(X2) == 1) or
+              (np.ndim(X1) == 1 and np.ndim(X2) > 1)):
             result = np.exp(
-                - np.linalg.norm(X - Y, axis=1) / (2 * self.sigma ** 2)
+                - np.linalg.norm(X1 - X2, axis=1) / (2 * self.sigma ** 2)
             )
 
-        elif np.ndim(X) > 1 and np.ndim(Y) > 1:
+        elif np.ndim(X1) > 1 and np.ndim(X2) > 1:
             result = np.exp(- np.linalg.norm(
-                X[:, np.newaxis] - Y[np.newaxis, :], axis=2
+                X1[:, np.newaxis] - X2[np.newaxis, :], axis=2
             ) / (2 * self.sigma ** 2))
 
         return result
@@ -231,6 +294,244 @@ class SVMClassifier:
 
         return np.sum(alphas) - 1 / 2 * np.sum(bulk)
 
+    def _take_step(self, index1, index2):
+        # Skip if chosen alphas are the same
+        if index1 == index2:
+            return 0
+
+        alpha1 = self.alphas[index1]
+        alpha2 = self.alphas[index2]
+
+        low, high = self._compute_bounds(index1, index2)
+        if (low == high):
+            return 0
+
+        a2 = self._compute_a2(index1, index2, low, high)
+        if self._not_optimizable(a2, alpha2):
+            return 0
+        a1 = self._compute_a1(index1, index2, a2)
+
+        b_new = self._compute_b(index1, index2, a1, a2)
+
+        # Update model object with new alphas & threshold
+        self.alphas[index1] = a1
+        self.alphas[index2] = a2
+
+        # Update error cache
+        self._update_error_cache(b_new, index1, index2, a1, a2, alpha1, alpha2)
+
+        # Update model threshold
+        self.b = b_new
+
+        return 1
+
+    def _examine_example(self, index2):
+        y2 = self.y[index2]
+        alpha2 = self.alphas[index2]
+        error2 = self.errors[index2]
+        r2 = error2 * y2
+
+        # Proceed if error is within specified tolerance (tol)
+        if ((r2 < -self.tol and
+             alpha2 < self.C) or (r2 > self.tol and alpha2 > 0)):
+
+            if (len(self.alphas[(self.alphas != 0) &
+                                (self.alphas != self.C)]) > 1):
+                # Use 2nd choice heuristic is choose max difference in error
+                if self.errors[index2] > 0:
+                    index1 = np.argmin(self.errors)
+                elif self.errors[index2] <= 0:
+                    index1 = np.argmax(self.errors)
+                step_result = self._take_step(index1, index2)
+                if step_result:
+                    return 1
+
+            # Loop through non-zero and non-C alphas, starting at a random
+            # point
+            for index1 in np.roll(
+                np.where((self.alphas != 0) & (self.alphas != self.C))[0],
+                np.random.choice(np.arange(self.X.shape[0]))
+            ):
+                step_result = self._take_step(index1, index2)
+                if step_result:
+                    return 1
+
+            # loop through all alphas, starting at a random point
+            for index1 in np.roll(
+                np.arange(self.X.shape[0]),
+                np.random.choice(np.arange(self.X.shape[0]))
+            ):
+                step_result = self._take_step(index1, index2)
+                if step_result:
+                    return 1
+
+        return 0
+
+    def _compute_bounds(self, index1, index2):
+        """
+        Compute lower and higher bounds on new possible alpha values
+        """
+        y1 = self.y[index1]
+        y2 = self.y[index2]
+        alpha1 = self.alphas[index1]
+        alpha2 = self.alphas[index2]
+
+        if y1 == y2:
+            low = max(0, alpha1 + alpha2 - self.C)
+            high = min(self.C, alpha1 + alpha2)
+        else:
+            low = max(0, alpha2 - alpha1)
+            high = min(self.C, self.C + alpha2 - alpha1)
+
+        return low, high
+
+    def _compute_eta(self, index1, index2):
+        """
+        Compute kernel & 2nd derivative eta
+        """
+        x1 = self.X[index1]
+        x2 = self.X[index2]
+
+        if self.kernel == 'linear':
+            k11 = self._linear_kernel(x1, x1)
+            k12 = self._linear_kernel(x1, x2)
+            k22 = self._linear_kernel(x2, x2)
+        elif self.kernel == 'gaussian':
+            k11 = self._gaussian_kernel(x1, x1)
+            k12 = self._gaussian_kernel(x1, x2)
+            k22 = self._gaussian_kernel(x2, x2)
+        eta = 2 * k12 - k11 - k22
+
+        return eta, k11, k12, k22
+
+    def _not_optimizable(self, a2, alpha2):
+        """
+        Check if a2 can be further optimized.
+        """
+        if np.abs(a2 - alpha2) < self.epsilon * (a2 + alpha2 + self.epsilon):
+            return True
+
+    def _compute_a1(self, index1, index2, a2):
+        """
+        Compute new alpha1 (a1)
+        """
+        y1 = self.y[index1]
+        y2 = self.y[index2]
+        alpha1 = self.alphas[index1]
+        alpha2 = self.alphas[index2]
+        a1 = alpha1 + y1 * y2 * (alpha2 - a2)
+
+        return a1
+
+    def _compute_a2(self, index1, index2, low, high):
+        """
+        Compute new alpha2 (a2)
+        """
+
+        y2 = self.y[index2]
+        alpha2 = self.alphas[index2]
+        error1 = self.errors[index1]
+        error2 = self.errors[index2]
+
+        eta = self._compute_eta(index1, index2)[0]
+
+        if eta < 0:
+            a2 = alpha2 - y2 * (error1 - error2) / eta
+            # Clip a2 based on bounds L & H
+            a2 = max(low, min(high, a2))
+
+        # If eta is non-negative, move new a2 to bound with greater objective
+        # function value
+        else:
+            alphas_copy = self.alphas.copy()
+            alphas_copy[index2] = low
+            # objective function output with a2 = L
+            low_objective = self._objective_function(alphas_copy)
+
+            alphas_copy[index2] = high
+            # objective function output with a2 = H
+            high_objective = self._objective_function(alphas_copy)
+
+            if low_objective > (high_objective + self.epsilon):
+                a2 = low
+            elif low_objective < (high_objective - self.epsilon):
+                a2 = high
+            else:
+                a2 = alpha2
+
+        # Push a2 to 0 or C if very close
+        if a2 < 1e-8:
+            a2 = 0.0
+        elif a2 > (self.C - 1e-8):
+            a2 = self.C
+
+        return a2
+
+    def _compute_b(self, index1, index2, a1, a2):
+        """
+        Update threshold b to reflect newly calculated alphas
+        Calculate both possible thresholds
+        """
+        y1 = self.y[index1]
+        y2 = self.y[index2]
+        alpha1 = self.alphas[index1]
+        alpha2 = self.alphas[index2]
+        error1 = self.errors[index1]
+        error2 = self.errors[index2]
+        _, k11, k12, k22 = self._compute_eta(index1, index2)
+
+        b1 = error1 + \
+            y1 * (a1 - alpha1) * k11 + y2 * (a2 - alpha2) * k12 + self.b
+        b2 = error2 + \
+            y1 * (a1 - alpha1) * k12 + y2 * (a2 - alpha2) * k22 + self.b
+
+        # Set new threshold based on if a1 or a2 is bounded by low and/or high
+        if 0 < a1 and a1 < self.C:
+            b_new = b1
+        elif 0 < a2 and a2 < self.C:
+            b_new = b2
+        # Average thresholds if both are bound
+        else:
+            b_new = (b1 + b2) * 0.5
+
+        return b_new
+
+    def _update_error_cache(self, b_new, index1, index2,
+                            a1, a2, alpha1, alpha2):
+        """
+        Update error cache
+        Error cache for optimized alphas is set to 0 if they're unbound
+
+        Returns:
+        -------
+        self : object
+        """
+        y1 = self.y[index1]
+        y2 = self.y[index2]
+
+        for index, alpha in zip([index1, index2], [a1, a2]):
+            if 0.0 < alpha < self.C:
+                self.errors[index] = 0.0
+
+        # Set non-optimized errors based on equation 12.11 in Platt's book
+        non_optimized = [n for n in range(self.X.shape[0])
+                         if (n != index1 and n != index2)]
+        if self.kernel == "linear":
+            correction = y1 * (a1 - alpha1) * \
+                self._linear_kernel(self.X[index1], self.X[non_optimized]) + \
+                y2 * (a2 - alpha2) * \
+                self._linear_kernel(self.X[index2], self.X[non_optimized]) + \
+                self.b - b_new
+        elif self.kernel == "gaussian":
+            correction = y1 * (a1 - alpha1) * \
+                self._gaussian_kernel(self.X[index1], self.X[non_optimized]) +\
+                y2 * (a2 - alpha2) * \
+                self._gaussian_kernel(self.X[index2], self.X[non_optimized]) +\
+                self.b - b_new
+        self.errors[non_optimized] = self.errors[non_optimized] + correction
+
+        return self
+
     def plot_decision_boundary(self, ax, resolution=100):
             """
             Plots the model's decision boundary on the input axes object.
@@ -246,7 +547,7 @@ class SVMClassifier:
             yrange = np.linspace(
                 self.X[:, 1].min(), self.X[:, 1].max(), resolution)
             grid = [[
-                self.predict(np.array([xr, yr])) for yr in yrange
+                self.decision_function(np.array([xr, yr])) for yr in yrange
             ] for xr in xrange]
             grid = np.array(grid).reshape(len(xrange), len(yrange))
 
@@ -270,169 +571,3 @@ class SVMClassifier:
             )
 
             return grid, ax
-
-    def _take_step(self, index_1, index_2):
-        # Skip if chosen alphas are the same
-        if index_1 == index_2:
-            return 0
-
-        alpha_1 = self.alphas[index_1]
-        alpha_2 = self.alphas[index_2]
-
-        y1 = self.y[index_1]
-        y2 = self.y[index_2]
-
-        error_1 = self.errors[index_1]
-        error_2 = self.errors[index_2]
-
-        sign = y1 * y2
-
-        # Compute lower and higher bounds on new possible alpha values
-        if y1 == y2:
-            low = max(0, alpha_1 + alpha_2 - self.C)
-            high = min(self.C, alpha_1 + alpha_2)
-        else:
-            low = max(0, alpha_2 - alpha_1)
-            high = min(self.C, self.C + alpha_2 - alpha_1)
-
-        if (low == high):
-            return 0
-
-        # Compute kernel & 2nd derivative eta
-        if self.kernel == 'linear':
-            k11 = self._linear_kernel(self.X[index_1], self.X[index_1])
-            k12 = self._linear_kernel(self.X[index_1], self.X[index_2])
-            k22 = self._linear_kernel(self.X[index_2], self.X[index_2])
-        elif self.kernel == 'gaussian':
-            k11 = self._gaussian_kernel(self.X[index_1], self.X[index_1])
-            k12 = self._gaussian_kernel(self.X[index_1], self.X[index_2])
-            k22 = self._gaussian_kernel(self.X[index_2], self.X[index_2])
-        eta = 2 * k12 - k11 - k22
-
-        # Compute new alpha 2 (a2) if eta is negative
-        if eta < 0:
-            a2 = alpha_2 - y2 * (error_1 - error_2) / eta
-            # Clip a2 based on bounds L & H
-            a2 = max(low, min(high, a2))
-
-        # If eta is non-negative, move new a2 to bound with greater objective
-        # function value
-        else:
-            alphas_copy = self.alphas.copy()
-            alphas_copy[index_2] = low
-            # objective function output with a2 = L
-            low_objective = self._objective_function(alphas_copy)
-
-            alphas_copy[index_2] = high
-            # objective function output with a2 = H
-            high_objective = self._objective_function(alphas_copy)
-
-            if low_objective > (high_objective + self.epsilon):
-                a2 = low
-            elif low_objective < (high_objective - self.epsilon):
-                a2 = high
-            else:
-                a2 = alpha_2
-
-        # Push a2 to 0 or C if very close
-        if a2 < 1e-8:
-            a2 = 0.0
-        elif a2 > (self.C - 1e-8):
-            a2 = self.C
-
-        # If examples can't be optimized within epsilon (eps), skip this pair
-        if np.abs(a2 - alpha_2) < self.epsilon * (a2 + alpha_2 + self.epsilon):
-            return 0
-
-        # Calculate new alpha 1 (a1)
-        a1 = alpha_1 + sign * (alpha_2 - a2)
-
-        # Update threshold b to reflect newly calculated alphas
-        # Calculate both possible thresholds
-        b1 = error_1 + \
-            y1 * (a1 - alpha_1) * k11 + y2 * (a2 - alpha_2) * k12 + self.b
-        b2 = error_2 + \
-            y1 * (a1 - alpha_1) * k12 + y2 * (a2 - alpha_2) * k22 + self.b
-
-        # Set new threshold based on if a1 or a2 is bounded by low and/or high
-        if 0 < a1 and a1 < self.C:
-            b_new = b1
-        elif 0 < a2 and a2 < self.C:
-            b_new = b2
-        # Average thresholds if both are bound
-        else:
-            b_new = (b1 + b2) * 0.5
-
-        # Update model object with new alphas & threshold
-        self.alphas[index_1] = a1
-        self.alphas[index_2] = a2
-
-        # Update error cache
-        # Error cache for optimized alphas is set to 0 if they're unbound
-        for index, alpha in zip([index_1, index_2], [a1, a2]):
-            if 0.0 < alpha < self.C:
-                self.errors[index] = 0.0
-
-        # Set non-optimized errors based on equation 12.11 in Platt's book
-        non_optimized = [n for n in range(self.X.shape[0])
-                         if (n != index_1 and n != index_2)]
-        if self.kernel == "linear":
-            correction = y1 * (a1 - alpha_1) * \
-                self._linear_kernel(self.X[index_1], self.X[non_optimized]) + \
-                y2 * (a2 - alpha_2) * \
-                self._linear_kernel(self.X[index_2], self.X[non_optimized]) + \
-                self.b - b_new
-        elif self.kernel == "gaussian":
-            correction = y1 * (a1 - alpha_1) * \
-                self._gaussian_kernel(self.X[index_1], self.X[non_optimized]) +\
-                y2 * (a2 - alpha_2) * \
-                self._gaussian_kernel(self.X[index_2], self.X[non_optimized]) +\
-                self.b - b_new
-        self.errors[non_optimized] = self.errors[non_optimized] + correction
-
-        # Update model threshold
-        self.b = b_new
-
-        return 1
-
-    def _examine_example(self, index_2):
-        y2 = self.y[index_2]
-        alpha_2 = self.alphas[index_2]
-        error_2 = self.errors[index_2]
-        r2 = error_2 * y2
-
-        # Proceed if error is within specified tolerance (tol)
-        if ((r2 < -self.tol and
-             alpha_2 < self.C) or (r2 > self.tol and alpha_2 > 0)):
-
-            if (len(self.alphas[(self.alphas != 0) &
-                                (self.alphas != self.C)]) > 1):
-                # Use 2nd choice heuristic is choose max difference in error
-                if self.errors[index_2] > 0:
-                    index_1 = np.argmin(self.errors)
-                elif self.errors[index_2] <= 0:
-                    index_1 = np.argmax(self.errors)
-                step_result = self._take_step(index_1, index_2)
-                if step_result:
-                    return 1
-
-            # Loop through non-zero and non-C alphas, starting at a random
-            # point
-            for index_1 in np.roll(
-                np.where((self.alphas != 0) & (self.alphas != self.C))[0],
-                np.random.choice(np.arange(self.X.shape[0]))
-            ):
-                step_result = self._take_step(index_1, index_2)
-                if step_result:
-                    return 1
-
-            # loop through all alphas, starting at a random point
-            for index_1 in np.roll(
-                np.arange(self.X.shape[0]),
-                np.random.choice(np.arange(self.X.shape[0]))
-            ):
-                step_result = self._take_step(index_1, index_2)
-                if step_result:
-                    return 1
-
-        return 0
